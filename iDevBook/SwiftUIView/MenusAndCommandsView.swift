5c957fd3e3d86b4dd72a0486a45299c16c7924d1
kaiyang0815 <wangkaiyang0815@gmail.com>
2025-03-18 19:54:11 +0800

    Added: add new menus and commands demo view

---
+++ iDevBook/SwiftUIView/MenusAndCommandsView.swift
@@ -0,0 +1,20 @@
+//
+//  MenusAndCommandsView.swift
+//  iDevBook
+//
+//  Created by kaiyang0815 on 2025/3/18.
+//  
+//    
+
+
+import SwiftUI
+
+struct MenusAndCommandsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenusAndCommandsView()
+}
